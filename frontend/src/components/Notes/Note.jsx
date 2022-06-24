839a051c2792b909b38195b291602610cede29ec
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-24 14:08:21 +0300

    created note page where will be displaying notes by hash

--- frontend/src/components/Notes/Note.jsx
+++ frontend/src/components/Notes/Note.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Create from '../Create/Create';
+import fetching from '../fetch/fetchObj';
 
 function Note() {
+
+  const [noteText,setNoteText] = useState('')
+
+  /* Destructuring the fetching object. */
+  const {handleNotes} = fetching
+
+  const {noteURL} = useParams()
+
+  useEffect(()=>{
+    if(noteURL != undefined){
+      handleNotes({"url":noteURL})
+        .then(data=>{
+          console.log('notes data >>>',data);
+          if(data.result){
+            setNoteText(data.note)
+          }
+      })
+    }
+  },[])
+
   return (
-    <div>Note</div>
+    <div>
+        <div>
+          {noteText}
+        </div>
+        
+    </div>
   )
 }
 
