037e6486d0478c447d3f4b2e92d6d4c063460435
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-24 14:27:09 +0300

    added form for searching notes by hash

--- frontend/src/components/Notes/Note.jsx
+++ frontend/src/components/Notes/Note.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React,{ useState,useEffect } from 'react'
 import { useParams } from 'react-router-dom';
-import Create from '../Create/Create';
 import fetching from '../fetch/fetchObj';
+import env from '../../env.json'
+
 
 function Note() {
 
   const [noteText,setNoteText] = useState('')
+  const [listClass,setListClass] = useState('hide')
+  const [formClass,setFormClass] = useState('row gy-2 gx-3 align-items-center')
+
 
   /* Destructuring the fetching object. */
   const {handleNotes} = fetching
@@ -23,13 +25,32 @@ function Note() {
             setNoteText(data.note)
           }
       })
+      setFormClass('hide')
+      setListClass('')
     }
   },[])
 
+  const handleHash = (e) =>{
+      e.preventDefault()
+      let url = e.target.elements.hash.value
+      if(url.value===0){
+        alert('Введите hash')
+      }
+      url.trim()
+      window.location.href = env.url+url
+  }
+
   return (
     <div>
-        <div>
+        <div className={listClass}>
           {noteText}
+        </div>
+        <div className={formClass}>
+          <form className={formClass} onSubmit={handleHash}>
+            <label htmlFor="hash">Введите хэш заметки</label>
+            <input type="text" name='hash' />
+            <button className='btn btn-dark'>Поиск</button>
+          </form>
         </div>
         
     </div>
